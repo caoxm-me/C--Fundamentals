4cc035ae192c0d87e3d57e2625b92c1024065a83
caoxm-me <caoxm1114@outlook.com>
2021-12-23 21:44:31 +0800

    finishing

---
+++ 46-ThreadPool/46-ThreadPool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _46_ThreadPool
+{
+   
+
+    class Program
+    {
+        static void SimpleTask(object o)
+        {
+            for(int i = 3; i > 0; --i)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // 线程池中的线程均是后台线程，进程不会等待其结束
+            ThreadPool.QueueUserWorkItem(new WaitCallback(SimpleTask));
+            // 卡住主线程，让进程不结束
+            Console.ReadLine();
+            Console.WriteLine("Quit");
+        }
+    }
+}
---
+++ 47-Timer/47-Timer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _47_Timer
+{
+    class Program
+    {
+        static void SimpleTask(object o)
+        {
+            for(int i = 3; i > 0; --i)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(150);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(new TimerCallback(SimpleTask), null, 1000, 1000);
+
+            // 定时器也是后台线程，需要卡住主线程以让进程不退出
+            Console.ReadLine();
+            // 定时器包含非托管资源，需要调用Dispose方法
+            timer.Dispose();
+            Console.WriteLine("Quit");
+        }
+    }
+}
